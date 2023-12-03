fb6a24ea083134c81ad0e4fafdaaf4d0d9d18b36
Hacedor2003 <bryanlenier@gmail.com>
2023-12-03 18:20:28 -0500

    Comparar entradas con BD

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse , JsonResponse
+from .models import Project,Task
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
 #Para los html
@@ -9,3 +11,10 @@ def hello(request, username):
     return HttpResponse("<h1>Hello %s</h1>" % username)
 def about(request):
     return HttpResponse("About")
+
+def projects(request):
+    projects = list(Project.objects.values())
+    return JsonResponse(projects,safe=False)
+def task(request,id):
+    task = get_object_or_404(Task, id=id)
+    return HttpResponse('task: %s' % task.title)
