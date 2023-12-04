e78fced4eebb21eca19cb92e86220fa630ba1f6d
Hacedor2003 <bryanlenier@gmail.com>
2023-12-03 21:31:35 -0500

    Enviar y recibir informacion

--- myapp/urls.py
+++ myapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('about/',views.about),
     path('projects/',views.projects),
     path('task/',views.task),
+    path('create_task/',views.create_task),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse , JsonResponse
 from .models import Project,Task
-from django.shortcuts import get_object_or_404,render
+from django.shortcuts import render , redirect
+from .forms import CreateNewTask
 # Create your views here.
 
 #Para los html
@@ -29,3 +29,10 @@ def task(request):
     return render(request,'task.html' , {
         'tasks':tasks
     })
+
+def create_task(request):
+    if request.method == 'GET':
+        return render(request,'create_task.html' , { 'form':CreateNewTask()})
+    else:
+        Task.objects.create(title=request.POST['title'], description = request.POST['description'], project_id = 2)
+        return redirect('/task')
